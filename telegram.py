3a7a99633b8f76d422487f42ddf1ad32f8fee66c
Abhishek Kumar <97353002+Abhishek-kumar0503@users.noreply.github.com>
2023-08-06 18:34:06 +0530

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -1,7 +1,7 @@
 import requests
 import re
 
-base_url = "https://api.telegram.org/bot6376673556:AAFUw21pWQ3vPv1BZLDtl2_Xf23iMEmYFLA"
+base_url="https://api.telegram.org/bot6376673556:AAFUw21pWQ3vPv1BZLDtl2_Xf23iMEmYFLA"
 
 def get_chat_id(update):
     return update["message"]["chat"]["id"]
@@ -16,29 +16,25 @@ def read_msg(offset):
     print(data)
     for result in data["result"]:
         message_text = result.get("message", {}).get("text")
+        chat_id = get_chat_id(result)
         if message_text:
-            global v
             recording_id = re.search(r'recordingId=(\d+)', message_text)
             if recording_id:
                 v = recording_id.group(1)
-                send_msg(get_chat_id(result), result)
+                send_msg(chat_id, f"https://static.smpopular.com/production/uploading/recordings/{v}/master.mp4")
             else:
-                send_msg("get_chat_id(result)", result)
+                send_msg(chat_id, "Invalid or missing recording ID. Please provide a valid link.")
 
     if data["result"]:
         return data["result"][-1]["update_id"] + 1
 
-def send_msg(chat_id, message):
-    message_id = message["message"]["message_id"]
-    text = f"https://static.smpopular.com/production/uploading/recordings/{v}/master.mp4"
+def send_msg(chat_id, text):
     if chat_id:
         parameter = {
             "chat_id": chat_id,
-            "reply_to_message_id": message_id,
-            "text": text
+            "text":  text if text else "This url will not exist please check it"
         }
-    resp = requests.get(base_url + "/sendMessage", data=parameter)
-    print(resp.text)
+        resp = requests.post(base_url + "/sendMessage", data=parameter)
 
 offset = 0
 while True:
