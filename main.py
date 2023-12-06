519307e72135f7891c11a646be3c861b5207694f
Abhishek Kumar <97353002+Abhishek-kumar0503@users.noreply.github.com>
2023-12-06 13:40:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,17 +15,30 @@ def is_valid_url(url):
     return re.match(r'^(http|https)://', url) is not None
 
 def read_msg(update):
-    message = update.get("message", {}).get("text")
-    start_index = message.find("recordingId=")
-    end_index = message.find("&", start_index)
-    chat_id = get_chat_id(update)
-    if start_index != -1 and end_index != -1:
-        recording_id = message[start_index + len("recordingId="):end_index]
-        print(recording_id)
-        video_url = f"https://static.smpopular.com/production/uploading/recordings/{recording_id}/master.mp4"
-        send_video(chat_id, video_url)
-    else:
-        send_msg(chat_id, "Invalid or missing recording ID. Please provide a valid link.")
+    message = result.get("message", {}).get("text")
+        print(message)
+        url_pattern = re.compile(r'https?://\S+')
+        urls = re.findall(url_pattern, message)
+        for url in urls:
+            print(url)
+        start_index = message.find("recordingId=")
+        end_index = message.find("&", start_index)
+        chat_id = get_chat_id(result)
+        if start_index != -1 and end_index != -1:
+            recording_id = message[start_index + len("recordingId="):end_index]
+            print(recording_id)
+            video_url = f"https://static.smpopular.com/production/uploading/recordings/{recording_id}/master.mp4"
+            html_text = requests.get(url).text
+            url_to_check = 'https://improxy.smpopular.com/tools/im/560/production/uploading/recordings'
+            # Check if the specific URL format is present in the HTML content
+            if url_to_check in html_text:
+                print("The webpage contains the specified URL format. It's likely a video.")
+                send_video(chat_id, video_url)
+            else:
+                print("The webpage does not contain the specified URL format. It's likely an audio.")
+                send_audio(chat_id, video_url)
+        else:
+            send_msg(chat_id, "Invalid or missing recording ID. Please provide a valid link.")
 
 def send_msg(chat_id, text):
     if chat_id:
@@ -36,6 +49,16 @@ def send_msg(chat_id, text):
         requests.post(base_url + "sendMessage", data=parameter)
 
 def send_video(chat_id, video_url):
+    if chat_id:
+        parameter = {
+            "chat_id": chat_id,
+        }
+        files = {
+            "video": requests.get(video_url).content
+        }
+        requests.post(base_url + "sendVideo", data=parameter, files=files)
+        
+def send_audio(chat_id, video_url):
     if chat_id:
         parameter = {
             "chat_id": chat_id,
